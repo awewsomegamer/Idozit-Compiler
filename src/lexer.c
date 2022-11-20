42844f48b9c127d64014aa43ee3a42ef1f874c17
awewsomegamer <>
2022-11-20 15:49:10 -0800

    Library working
    I got the compilation to a library working and tested.
    I just need to finish up the numbers portion of the lexer now.

--- src/lexer.c
+++ src/lexer.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-
+#include <ctype.h>
 
 char *expression_string = NULL;
 char **variables_list = NULL;
@@ -10,7 +10,7 @@ uint32_t variable_count = 0;
 
 uint32_t expression_ptr = 0;
 
-inline char next_char() {
+char next_char() {
     return *(expression_string + expression_ptr++);
 }
 
@@ -39,56 +39,60 @@ char* get_string(char c) {
 }
 
 int lex(token_t* token) {
-    char c = next_solid_char();
+    printf("LEX CALLED\n");
+    // char c = next_solid_char();
 
-    switch (c) {
-    case '+':
-        token->type = T_ADD;
-        return 1;
+    // switch (c) {
+    // case '+':
+    //     token->type = T_ADD;
+    //     return 1;
 
-    case '-':
-        token->type = T_SUB;
-        return 1;
+    // case '-':
+    //     token->type = T_SUB;
+    //     return 1;
 
-    case '*':
-        token->type = T_MUL;
-        return 1;
+    // case '*':
+    //     token->type = T_MUL;
+    //     return 1;
 
-    case '/':
-        token->type = T_DIV;
-        return 1;
+    // case '/':
+    //     token->type = T_DIV;
+    //     return 1;
 
-    case '(':
-        token->type = T_LPAREN;
-        return 1;
+    // case '(':
+    //     token->type = T_LPAREN;
+    //     return 1;
 
-    case ')':
-        token->type = T_RPAREN;
-        return 1;
+    // case ')':
+    //     token->type = T_RPAREN;
+    //     return 1;
 
-    default:
-        char* string = get_string(c);
+    // default:
+    //     char* string = get_string(c);
 
-        for (int i = 0; i < variable_count; i++)
-            if (strcmp(string, variables_list[i]) == 0) {
-                token->type = T_VAR;
-                token->value = i;
-                return 1;
-            }
+    //     for (int i = 0; i < variable_count; i++)
+    //         if (strcmp(string, variables_list[i]) == 0) {
+    //             token->type = T_VAR;
+    //             token->value = i;
+    //             return 1;
+    //         }
 
-        for (int i = 0; i < T_FUNC_MAX; i++)
-            if (strcmp(string, T_FUNC_NAMES[i]) == 0) {
-                token->type = T_IDENT;
-                token->value = i;
-                return 1;
-            }
+    //     for (int i = 0; i < T_FUNC_MAX; i++)
+    //         if (strcmp(string, T_FUNC_NAMES[i]) == 0) {
+    //             token->type = T_IDENT;
+    //             token->value = i;
+    //             return 1;
+    //         }
 
-        // Number:
-        // Look at the string, while you don't reach a . you are looking at a whole number
-        // When you reach a . then flip over to the other integer, then put these two integers together
-        // To form a single double value
+    //     // Number:
+    //     // Look at the string, while you don't reach a . you are looking at a whole number
+    //     // When you reach a . then flip over to the other integer, then put these two integers together
+    //     // To form a single double value
 
-    }
+    //     if (isdigit(*string)) {
+            
+    //     }
+    // }
 
     return 0;
 }
--- src/library.c
+++ src/library.c
@@ -2,5 +2,6 @@
 #include <lexer.h>
 
 context_t expression(const char *form, ...) {
-        printf("Hello Library World %s", form);
+        printf("Hello Library World %s\n", form);
+        lex(NULL);
 }
