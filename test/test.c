dda37149709478e92f3078d51c723a48c13a249b
awewsomegamer <>
2022-12-28 17:17:20 -0800

    Test

--- test/test.c
+++ test/test.c
@@ -4,5 +4,7 @@ int main() {
         code_block_t code = compile(expression("INTEGRAL 1 x ( x )", 1, "x"));
 	printf("Area under x^2 - 4 from -2 to 2 is: %f\n", (run(code, 2.0) - run(code, -1.0)));
 
+        printf("Test\n");
+
         return 0;
 }
