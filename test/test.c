5f118d2b7b24f0f67b1d5941eadadc60d4fc7b96
awewsomegamer <>
2023-01-16 14:59:49 -0800

    Small time correction

--- test/test.c
+++ test/test.c
@@ -3,7 +3,7 @@
 int main() {
         idozit_word.caching = 1;
         idozit_word.available_caches = 5;
-        idozit_word.cache_cut_off = 60 * 60 * 5 * 1000; // 5 minutes
+        idozit_word.cache_cut_off = 60 * 5 * 1000; // 5 minutes
 
         code_block_t code = compile(expression("INTEGRAL 2 x ( x )", 1, "x"));
         // TODO: Really annoying how run function doesn't like integers and only likes when the .0 is added
